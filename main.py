25883966904deada9850708dc3b87ee29c03f081
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-01-04 20:45:55 +0100

    chatbot logic

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+from googlesearch import search
+
+
+# chatbot logic
+def bot():
+
+    # user input
+    user_msg = request.values.get('Body', '').lower()
+
+    # creating object of MessagingResponse
+    response = MessagingResponse()
+
+    # user query
+    q = user_msg + "geeksforgeeks.org"
+
+    # list to store urls
+    results = []
+
+    # searching and storing urls
+    for i in search(q, tld='co.in', num=6, stop=6, pause=2):
+        results.append(i)
+
+    # displaying result
+    msg = response.message(f"--- Results for '{user_msg}' --- ")
+    for result in results:
+        msg = response.message(result)
+
+    return str(response)
+
